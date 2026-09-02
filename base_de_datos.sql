e56289579a5e5193251905de493fd6ce824bdeb5
manuel124i874 <6263805@incb.edu.sv>
2026-09-02 17:32:29 -0600

    Inicio del módulo de compras: Agregada tabla y vista de proveedores

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -100,3 +100,6 @@ SELECT
 FROM productos p
 INNER JOIN categorias c ON p.categoria_id = c.id 
 WHERE c.nombre_categoria = 'Accesorios';
+INSERT INTO proveedores (nombre_empresa, contacto, telefono, direccion) VALUES
+('Tech Data El Salvador', 'Juan Pérez', '2255-8899', 'San Salvador, Col. Escalón'),
+('Distribuidora de Papel', 'María Gómez', '2666-4433', 'San Miguel, Centro');
